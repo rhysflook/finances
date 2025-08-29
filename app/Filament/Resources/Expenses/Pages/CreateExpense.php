491469f9b98d23f0980aae7ebc6142eeeccb0676
Rhys Flook <rhys@Rhys.localdomain>
2025-08-30 07:16:32 +0900

    create multi item input for expenses

--- app/Filament/Resources/Expenses/Pages/CreateExpense.php
+++ app/Filament/Resources/Expenses/Pages/CreateExpense.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Expenses\Pages;
 
 use App\Filament\Resources\Expenses\ExpenseResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateExpense extends CreateRecord
 {
@@ -12,5 +13,21 @@ class CreateExpense extends CreateRecord
     protected function preserveFormDataWhenCreatingAnother(array $data): array
     {
         return $data;
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $model = static::getModel();
+        $to_return = null;
+        foreach ($data['items'] as $item) {
+            $to_return = $model::create([
+                ...$item,
+                'vendor' => $data['vendor'],
+                'description' => $data['description'],
+                'currency_id' => $data['currency_id'],
+                'date' => $data['date'],
+            ]);
+        }
+        return $to_return;
     }
 }
--- app/Filament/Resources/Expenses/Schemas/ExpenseForm.php
+++ app/Filament/Resources/Expenses/Schemas/ExpenseForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Expenses\Schemas;
 
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
@@ -13,14 +14,22 @@ class ExpenseForm
     {
         return $schema
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                TextInput::make('amount')
-                    ->required()
-                    ->numeric(),
-                Select::make('category_id')
-                    ->required()
-                    ->relationship('category', 'name'),
+                Repeater::make('items')
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+                        TextInput::make('amount')
+                            ->required()
+                            ->numeric(),
+                        Select::make('category_id')
+                            ->required()
+                            ->relationship('category', 'name'),
+                    ])
+                    ->columns(3)
+                    ->defaultItems(1)
+                    ->minItems(1)
+                    ->columnSpanFull(),
+                
                 // Select::make('sub_category_id')
                 //     ->relationship('subCategory', 'name'),
                 TextInput::make('vendor'),
